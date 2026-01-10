4c53692ad8573f1c972ea84725f9f6c811830879
Remco <61921490+YosoraLife@users.noreply.github.com>
2026-01-10 19:32:54 +0100

    Update NodeJS installation script to include bzip2

--- jukebox-install.sh
+++ jukebox-install.sh
@@ -72,7 +72,7 @@ G_AGI unclutter && echo '/usr/bin/unclutter -idle 0.1 &' > /etc/chromium.d/dietp
 cd ~
 
 # Install NodeJS
-sudo apt-get install -y ca-certificates curl gnupg && sudo mkdir -p /etc/apt/keyrings
+sudo apt-get install -y ca-certificates curl bzip2 gnupg && sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main | sudo tee /etc/apt/sources.list.d/nodesource.list
